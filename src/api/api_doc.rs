bb5e7f65807a81147fbf2062249899b0f4907ea5
Roman Zanko <roman.zanko.vl@gmail.com>
2025-02-05 13:11:42 +0400

    Add logic for update patient endpoint

--- src/api/api_doc.rs
+++ src/api/api_doc.rs
@@ -1,5 +1,9 @@
 use utoipa::OpenApi;
 
 #[derive(OpenApi)]
-#[openapi(paths(crate::api::routes::create_patient, crate::api::routes::get_patient))]
+#[openapi(paths(
+    crate::api::routes::create_patient,
+    crate::api::routes::get_patient,
+    crate::api::routes::update_patient
+))]
 pub struct ApiDoc;
