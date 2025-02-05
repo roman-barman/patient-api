b3669cf9ea7a9a9b0788aaab677cc3bf17e0ea54
Roman Zanko <roman.zanko.vl@gmail.com>
2025-02-05 12:00:50 +0400

    Add update patient command handler

--- src/domain/version.rs
+++ src/domain/version.rs
@@ -1,4 +1,5 @@
-use chrono::{DateTime, Local};
+use chrono::{DateTime, Local, TimeZone};
+use thiserror::Error;
 
 #[derive(Debug, Clone)]
 pub struct Version(DateTime<Local>);
@@ -27,8 +28,25 @@ impl From<Version> for i64 {
     }
 }
 
+impl TryFrom<i64> for Version {
+    type Error = VersionValidationError;
+    fn try_from(value: i64) -> Result<Self, Self::Error> {
+        let result = Local.timestamp_millis_opt(value).single();
+        match result {
+            Some(value) => Ok(Self(value)),
+            None => Err(VersionValidationError::InvalidVersion),
+        }
+    }
+}
+
 impl AsRef<DateTime<Local>> for Version {
     fn as_ref(&self) -> &DateTime<Local> {
         &self.0
     }
+}
+
+#[derive(Error, Debug, PartialEq, Eq)]
+pub enum VersionValidationError {
+    #[error("invalid version")]
+    InvalidVersion,
 }
