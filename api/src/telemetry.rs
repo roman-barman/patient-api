91dc0690c44b5cd7b1e60547149cf90428465512
Roman Zanko <roman.zanko.vl@gmail.com>
2025-02-13 10:27:03 +0400

    Add docker compose

--- api/src/telemetry.rs
+++ api/src/telemetry.rs
@@ -1,19 +1,25 @@
 use opentelemetry::trace::TracerProvider;
 use opentelemetry::KeyValue;
+use opentelemetry_otlp::WithExportConfig;
 use opentelemetry_sdk::Resource;
 use tracing::subscriber::set_global_default;
 use tracing_log::LogTracer;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::{fmt, EnvFilter, Registry};
 
-pub fn initialize_tracing_subscribe(env_filter: String, name: String) {
+pub fn initialize_tracing_subscribe(env_filter: String, name: String, address: Option<String>) {
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
 
+    let span_builder = opentelemetry_otlp::SpanExporter::builder().with_tonic();
+    let span_builder = match address {
+        Some(val) => span_builder.with_endpoint(val),
+        None => span_builder,
+    };
+
     let tracer = opentelemetry_sdk::trace::TracerProvider::builder()
         .with_batch_exporter(
-            opentelemetry_otlp::SpanExporter::builder()
-                .with_tonic()
+            span_builder
                 .build()
                 .expect("Failed to build the opentelemetry tracer"),
             opentelemetry_sdk::runtime::Tokio,
