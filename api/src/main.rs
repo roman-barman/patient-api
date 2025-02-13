1bcb44f55d2224d16f825d06994db557c7efb8f5
Roman Zanko <roman.zanko.vl@gmail.com>
2025-02-13 10:27:03 +0400

    Add docker compose

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,11 +1,20 @@
 use crate::telemetry::initialize_tracing_subscribe;
 use lib_api::{Application, Settings};
+use std::env;
 
 mod telemetry;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    initialize_tracing_subscribe("info".into(), "patient-api".into());
+    let variable = "APP_TRACING_ADDRESS";
+    let env = env::var(variable);
+
+    let tracing_address = match env {
+        Ok(val) => Some(val),
+        Err(_e) => None,
+    };
+
+    initialize_tracing_subscribe("info".into(), "patient-api".into(), tracing_address);
     let settings = Settings::read_configuration()?;
     let application = Application::start(settings).await?;
     let _ = tokio::spawn(application.run_until_stopped()).await?;
